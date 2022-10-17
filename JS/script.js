6923294944be7b6f5dfcfad7d6a0fbba69963e98
Yarork <fabris.sa97@gmail.com>
2022-10-17 15:21:50 -0300

    segunda entrega JS

--- JS/script.js
+++ JS/script.js
@@ -1,101 +1,74 @@
-let nombreProductoA = "REMERA AMARILLAS"
-let precioProductoA = 100
-let stockProductoA = 40
+function Producto(nombre, precio, stock){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+}
 
-let nombreProductoX = "REMERA GRIS"
-let precioProductoX = 100
-let stockProductoX = 40
+let productoA = new Producto("camisetas", 100, 102)
+let productoB = new Producto("camperas", 80, 243)
+let productoC = new Producto("remeras amarillas", 60, 151)
+let productoD = new Producto("tazas", 140, 0)
+let productoE = new Producto("bolsas", 30, 53)
 
-let nombreProductoB = "camisetas"
-let precioProductoB = 80
-let stockProductoB = 100
+let listaProductos = [productoA, productoB, productoC, productoD, productoE]
 
-let nombreProductoC = "tazas"
-let precioProductoC = 30
-let stockProductoC = 20
+let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0)
 
-let nombreProductoD = "CAMPERAS"
-let precioProductoD = 110
-let stockProductoD = 60
+let listaNombres = listaProductosConStock.map((prod) => prod.nombre)
 
-let nombreProductoE = "BOLSAS"
-let precioProductoE = 30
-let stockProductoE = 50
+
+alert("En nuestra tienda, usted puede encontrar los siguientes productos con stock: \n - " + listaNombres.join("\n - "))
 
 function bienvenida(nombre, apellido){
     alert("Bienvenido " + nombre + " " + apellido)
 }
 
-function calcularIva(a){
-    precioConIva = a * 1.21
-    alert("El precio total de la compra con IVA es de " + precioConIva)
+function calcularIva(precioTotal){
+    precioConIva = precioTotal + precioTotal * 0.21
+    alert("El precio total de la compra con IVA (21%) es de " + precioConIva)
 }
 
-function precioCuota(a){
-    precioConIva = a / 3
-    alert("El precio total de la compra con IVA es de: " + a + " y cada cuota es de: " + precioConIva)
+function calculoPrecio(cantidad, precio){
+    precioTotal += (cantidad * precio)
 }
 
+function calculoStock(cantidad, stock, precio){
+    if(cantidad <= stock){
+        calculoPrecio(cantidad, precio)
+    }
+    else{
+        alert("Actualmente tenemos " + stock + " unidades de este producto")
+    }
+}
 nombre = prompt("Ingrese su nombre: ")
 apellido = prompt("Ingrese su apellido: ")
 
 bienvenida(nombre, apellido)
 
 let precioTotal = 0
-let precioC = 0
-let precioConIva = 0
 
-let texto = prompt("Ingrese SI para continuar con la compra \nIngrese NO para salir de la tienda")
+let texto = prompt("Ingrese SI para continuar con la compra\n Ingrese NO para salir de la tienda").toUpperCase()
 if(texto == "SI"){
-    let compraRemeras = prompt("Ingrese el numero de " + nombreProductoA + " que quiera comprar, su valor unitario es de " + precioProductoA)
-    if(compraRemeras <= stockProductoA){
-        precioTotal = precioTotal + compraRemeras * precioProductoA
-    }
-    else{
-        alert("No contamos con tanto stock en estos momentos")
-    }
+    let compraProductoA = prompt("Ingrese el numero de " + productoA.nombre + " que quiera agregar al carrito, su valor es de " + productoA.precio)
+    calculoStock(compraProductoA, productoA.stock, productoA.precio)
 
-    let compraRemeras2 = prompt("Ingrese la cantidad de " + nombreProductoX + " que desee comprar, su valor unitario es de " + precioProductoX)
-    if(compraRemeras2 <= stockProductoX){
-        precioTotal = precioTotal + compraRemeras2 * precioProductoX
-    }
-    else{
-        alert("No contamos con tanto stock en estos momentos")
-    }
-    
-    let compraPantalones = prompt("Ingrese el numero de " + nombreProductoB + " que quiera comprar, su valor unitario es de " + precioProductoB)
-    if(compraPantalones <= stockProductoB){
-        precioTotal = precioTotal + compraPantalones * precioProductoB
-    }
-    else{alert("No contamos con tanto stock en estos momentos")}
+    let compraProductoB = prompt("Ingrese el numero de " + productoB.nombre + " que quiera agregar al carrito, su valor es de " + productoB.precio)
+    calculoStock(compraProductoB, productoB.stock, productoB.precio)
 
-    let compraShorts = prompt("Ingrese el numero de " + nombreProductoC + " que quiera comprar, su valor unitario es de " + precioProductoC)
-    if(compraShorts <= stockProductoC){
-        precioTotal = precioTotal + compraShorts * precioProductoC
-    }
-    else{alert("No contamos con tanto stock en estos momentos")}
+    let compraProductoC = prompt("Ingrese el numero de " + productoC.nombre + " que quiera agregar al carrito, su valor es de " + productoC.precio)
+    calculoStock(compraProductoC, productoC.stock, productoC.precio)
 
-    let compraCamperas = prompt("Ingrese el numero de " + nombreProductoD + " que quiera comprar, su valor unitario es de " + precioProductoD)
-    if(compraCamperas <= stockProductoD){
-        precioTotal = precioTotal + compraCamperas * precioProductoD
-    }
-    else{alert("No contamos con tanto stock en estos momentosS")}
+    let compraProductoD = prompt("Ingrese el numero de " + productoD.nombre + " que quiera agregar al carrito, su valor es de " + productoD.precio)
+    calculoStock(compraProductoD, productoD.stock, productoD.precio)
 
-    let compraGorros = prompt("Ingrese el numero de " + nombreProductoE + " que quiera comprar, su valor unitario es de " + precioProductoE)
-    if(compraGorros <= stockProductoE){
-        precioTotal = precioTotal + compraGorros * precioProductoE
-    }
-    else{alert("No contamos con tanto stock en estos momentos")
-}
+    let compraProductoE = prompt("Ingrese el numero de " + productoE.nombre + " que quiera agregar al carrito, su valor es de " + productoE.precio)
+    calculoStock(compraProductoE, productoE.stock, productoE.precio)
 
-    alert("El precio total comprando en efectivo es de " + precioTotal )
+    alert("El precio total de la compra sin IVA es de " + precioTotal )
 
     calcularIva(precioTotal)
-    
-    precioCuota (precioConIva)
-
-    alert("Gracias por comprarnos a nosotros!!")
+    alert("Gracias por su compra!")
 }
 else{
-    alert("Que tengas buen dia!")
+    alert("Gracias por su respuesta")
 }
